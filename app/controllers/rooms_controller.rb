a68d10be8c620c8275d801220733f64658bc5b2a
kkazum <49956319+kkazum@users.noreply.github.com>
2021-01-04 21:21:43 +0900

    create new room

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,10 +1,21 @@
 class RoomsController < ApplicationController
   def new
+    @room = Room.new
   end
 
   def show
   end
 
   def create
+    @room = Room.new(room_params)
+    if @room.save
+      redirect_to @room
+    end
+  end
+
+  private
+
+  def room_params
+    params.require(:room).permit(:token)
   end
 end
