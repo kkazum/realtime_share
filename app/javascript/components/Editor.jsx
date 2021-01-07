a4942084ba3823b575e253fd7d8f08951161a6ee
kkazum <49956319+kkazum@users.noreply.github.com>
2021-01-07 20:01:30 +0900

    react markdown

--- app/javascript/components/Editor.jsx
+++ app/javascript/components/Editor.jsx
@@ -1,12 +1,21 @@
-import React from "react"
-import PropTypes from "prop-types"
-class Editor extends React.Component {
-  render () {
-    return (
-      <React.Fragment>
-      </React.Fragment>
-    );
-  }
+import React, { useState } from 'react'
+import * as ReactMarkdown from 'react-markdown'
+
+const Editor = () => {
+  const [text, setText] = useState()
+  return (
+    <>
+      <div className="left-content">
+        <form className="create-message-button">
+          <textarea value={ text } onChange={(e) => {setText(e.target.value)}} type="text" placeholder="共有したいメッセージを入力" className="input-message-area" />
+        </form>
+        <div className="preview">
+          <ReactMarkdown source={text} />
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default Editor
+
---
+++ app/javascript/components/Line.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const Line = () => {
+  return (
+    <>
+      <div className="right-content">
+        <div className="line__container">
+
+          <div className="line__title">
+            Share Area
+          </div>
+
+          <div className="line__contents">
+            <div className="line__left">
+              <div className="line__left-text">
+                <div className="name">neko</div>
+                <div className="text">nekonekonekonekonekonekonekoneko</div>
+              </div>
+            </div>
+
+            <div className="line__right">
+              <div className="text">dogdogdogdogdogdogdogdogdogdogdogdogdogdogdog</div>
+              <span className="date">既読<br />0:30</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Line
